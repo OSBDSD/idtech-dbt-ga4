56cd5d103606a72aff4de656b282c10bb750ef25
eric@opensourcebrokers.ca <eric@opensourcebrokers.ca>
2024-03-27 16:39:42 +0000

    Test Commit change for Erik

--- models/GA4_analysis/new_user_count.sql
+++ models/GA4_analysis/new_user_count.sql
@@ -1,5 +1,5 @@
-# ---Base table 1: count new user per page_path per pt date.
+# ---Base table 1: count new user per page_path per pt date. Test Change Comment
 
-select page_path, event_date_dt,  count(distinct client_key) as new_user_count  from 
-{{ref('stg_ga4__event_first_visit')}}
- group by page_path, event_date_dt
+select page_path, event_date_dt, count(distinct client_key) as new_user_count
+from {{ ref("stg_ga4__event_first_visit") }}
+group by page_path, event_date_dt
